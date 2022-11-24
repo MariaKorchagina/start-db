b5a4675302550f1505bdfd6c092754810fb4e58a
Maria Korchagina <kobysheva_maria@mail.ru>
2022-11-24 14:19:21 +0300

    added life cycles

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SwapiService from '../../services/swapi-service';
 import './random-planet.css';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 export default class RandomPlanet extends Component {
 
   swapiService = new SwapiService();
@@ -11,65 +12,64 @@ export default class RandomPlanet extends Component {
     loading: true
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 2500)
+    // компонент создает ссылку на метод в setInterval и даже после того , как этот компонент пропадает эта ссылка не очищается, т.е. setInterval продолжает работать и обновлять планеты.
+    //Перед тем , как кокмпонент будет удален , необходимо очистить ресурсы
+    // 
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.interval);
   }
 
   onPlanetLoaded = (planet) => {
     this.setState({
       planet,
-      loading: false
+      loading: false,
+      error: false
     });
   };
 
-  updatePlanet() {
-    // const id = Math.floor(Math.random() * 25) + 2;
-    const id = 12;
+  onError = (err) => {
+    this.setState({
+      error: true,
+      loading: false
+    })
+  }
+
+  updatePlanet = () => {
+    console.log('update')
+    const id = Math.floor(Math.random() * 25) + 3;
+    // 3 - т.к. в API нет 2ой планеты
     this.swapiService
       .getPlanet(id)
-      .then(this.onPlanetLoaded);
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
 
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
+
+    const hasData = !(loading || error);
+    const errorMessage = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet} /> : null;
+    const content = hasData ? <PlanetView planet={planet} /> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
         {spinner}
         {content}
-
-        {/* <Spinner />
-        <img className="planet-image"
-          src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-        <div>
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>{population}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Rotation Period</span>
-              <span>{rotationPeriod}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Diameter</span>
-              <span>{diameter}</span>
-            </li>
-          </ul>
-        </div> */}
       </div>
     );
   }
 }
 
 const PlanetView = ({ planet }) => {
-  const { id, name, population,
-    rotationPeriod, diameter } = planet;
+  const { id, name, population, rotationPeriod, diameter } = planet;
 
   return (
     <React.Fragment>
